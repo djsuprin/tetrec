f3bb6267b63640dcbd28ad9de591d87c5691e470
Vladimir Vasilyev <djsuprin@gmail.com>
2014-06-25 17:23:08 +0200

    added forgotten files

---
+++ js/i18n.js
@@ -0,0 +1,21 @@
+// Translations for UI strings
+var i18n = {
+	'en' : {
+		'start' : 'Start again',
+		'finish' : 'Finish',
+		'tetrec' : 'Tetrec',
+		'paused' : 'Tetrec paused',
+		'lines' : 'Lines',
+		'level' : 'Level',
+		'author' : 'Vladimir Vasilyev, 2014'
+	},
+	'ru' : {
+		'start' : 'Начать заново',
+		'finish' : 'Закончить',
+		'tetrec' : 'Тетрец',
+		'paused' : 'Тетрец на паузе',
+		'lines' : 'Линии',
+		'level' : 'Уровень',
+		'author' : 'Владимир Васильев, 2014'
+	}
+};
